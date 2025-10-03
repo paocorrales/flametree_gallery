797c991adc24d1d0754c34951abd485e3c4446dd
Evil-SrEnT <miailhematias06@gmail.com>
2025-10-03 11:10:45 -0300

    tree_Mati added

---
+++ template_script_matias.R
@@ -0,0 +1,26 @@
+# install.packages('flametree')
+library(flametree)
+
+# your name, without spaces or special character
+name <- "Mati"
+
+# pick a seed
+this_seed <- 838485
+
+# pick some colours
+shades <- c("cyan", "magenta", "orange", "green")
+
+# data structure defining the trees
+dat <- flametree_grow(seed = this_seed, time = 10, trees = 10)
+
+# draw the plot
+tree <- dat %>%
+  flametree_plot(
+    background = "black",
+    palette = shades,
+    style = "nativeflora"
+  )
+
+tree
+# save the plot
+flametree_save(tree, filename = paste0("fig/tree_", name, ".png"))
